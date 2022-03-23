c55a0f2b72b789ffeb39bc0d8dbe63ef3064351a
ak nabil <nabil786.ak@gmail.com>
2022-03-23 20:37:15 +0600

    add to card update

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -4,15 +4,20 @@ import './Product.css'
 const Product = (props) => {
     const {name,img,price,stock,ratings,seller,ratingsCount} = props.product;
     return (
-        <div> 
+        <div className='product-data'> 
             <div className='product-img'>
                 <img src={img} alt=''></img>
             </div>
             <div className='product-cart-info'>
-                <p className='product-name'>{name}</p>
-                <p>Price:{price}</p>
-
+                <h6 className='product-name'> {name}</h6>
+                <p>Price: ${price}</p>
+                <p><small>Manufacturer : {seller}</small></p>
+                <p><small>Rating : {ratings}</small></p> 
             </div>
+            
+                <button onClick={() => props.eventHendler(props.product)} className='crt-button'><p>Add to Cart</p> <i class="fa-solid fa-cart-plus"></i></button>
+            
+
         </div>
     );
 };
--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -4,11 +4,18 @@ import './Shop.css'
 
 const Shop = () => {
     const [products , setProducts] = useState([]);
+    const [cart , setCart] = useState([])
     useEffect(()=>{
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
     },[])
+    const eventHendleAddToCart = (product) => {
+        console.log(product)
+        const newCart = [...cart,product]
+        setCart(newCart);
+
+    }
     return (
         <main>
             <section className='shop-container'>
@@ -17,11 +24,20 @@ const Shop = () => {
                     products.map(product => <Product 
                         key={product.id}
                         product ={product}
+                        eventHendler ={eventHendleAddToCart}
                     ></Product>)
                 }
             </div>
-            <div>
+            <div className='order-container'>
                 <h2>Order Summary</h2>
+                <div className='order-info'>
+                <p>Selected Items: {cart.length}</p>
+                <p>Total Price: 0 </p>
+                <p>Total Shipping Charge: 0 </p>
+                <p>Tax: 0 </p>
+                <h3>Grand Total: 0</h3>
+                </div>
+
             </div>
             </section> 
 
